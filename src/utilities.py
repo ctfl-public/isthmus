b6286d6af6f899c8463e566f8f15d2c38555f845
Ahmed H Yassin <ahya224@g.uky.edu>
2025-09-03 14:28:51 -0400

    Add tiff_slicer() to utilities

--- src/utilities.py
+++ src/utilities.py
@@ -1,5 +1,60 @@
 import numpy as np
 import sys
+import imageio
+
+def tiff_slicer(fileName, x, y, z, l, voxel_size, lb= 5):
+    """
+    Slices a 3D TIFF image into a smaller voxelized cube.
+
+    Args:
+        fileName (str): Path to the input TIFF file.
+        x (float): x in the in-plane direction
+        y (float): y in the in-plane direction
+        z (float): z is the through thickness direction
+        l (float): cube length
+        voxel_size (float): size of the voxel
+        lb (float): buffer length
+
+    Returns:
+        tuple: (voxs, lims) A tuple containing the voxel coordinates and the limits of the sliced region.
+    """
+    lo = [-lb, -lb, -lb]  
+
+    hi = [l/2 + lb, (l + lb), (l + lb)]
+
+    lims = np.array([lo, hi])
+
+    voxs = []
+    voxelMTX = loadData(fileName)
+    sample_mtx = voxelMTX[int(z):int(z+l/2),int(y-l/2):int(y + l/2),int(x-l/2):int(x + l/2)]
+
+    for i in range(int(l)):
+        for j in range(int(l)):
+            for k in range(int(l/2)):
+                if sample_mtx[k,j,i] == 1:
+                    voxs.append([k,j,i])
+
+    lims = lims*voxel_size
+    voxs = np.array(voxs)*voxel_size
+
+    return voxs, lims
+
+def loadData(surf):
+    if surf.endswith('.tif'):
+        # Load the TIFF file
+        image_volume = imageio.volread(surf)
+    
+    elif surf.endswith('.txt') or surf.endswith('.dat'):
+        tempdata = np.loadtxt(surf, skiprows=2)
+        xmax = int(max(tempdata[:, 0]))
+        ymax = int(max(tempdata[:, 1]))
+        zmax = int(max(tempdata[:, 2]))
+        image_volume = np.zeros((xmax, ymax, zmax), dtype='int')
+        for val in tempdata:
+            image_volume[int(val[0]) - 1, int(val[1]) - 1, int(val[2]) - 1] = int(val[3])
+            
+    return image_volume
+
 
 def progress_bar(c, total, message):
     """
