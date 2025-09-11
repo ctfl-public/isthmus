d6e412a8bd9738a0ab0594c7a47dd1f5ec580534
Ahmed H Yassin <ahya224@g.uky.edu>
2025-09-11 13:22:40 -0400

    Add read func to voxel tri association to utilities

--- src/utilities.py
+++ src/utilities.py
@@ -105,3 +105,45 @@ def noise_gen(mag):
     Generates a random noise factor within ±(mag/100) of 1.
     """
     return (2*np.random.rand() - 1)*(mag/100) + 1
+
+
+def readVoxelTri(fileName):
+    """
+    Reads a voxel-triangle association file.
+    Returns mappings of triangles to their associated voxels and scalar fractions.
+    """
+    f = open(fileName)
+    tv_lines = f.readlines()
+    f.close()
+    tv_split = [tv.split() for tv in tv_lines]
+    triangle_ids = []
+    owned_voxels = []
+    owned_sfracs = []
+    c_voxels = []
+    c_scalar_fracs = []
+    tri_flag = 0
+    for i in range(1, len(tv_split)):
+        if (tv_split[i]):
+            if (tv_split[i][0] == 'start'):
+                triangle_ids.append(int(tv_split[i][-1]))
+                c_voxels = []
+                c_scalar_fracs = []
+                tri_flag = 1
+            elif (tv_split[i][0] == 'end'):
+                owned_voxels.append(c_voxels)
+                owned_sfracs.append(c_scalar_fracs)
+                tri_flag = 0
+            elif (tri_flag == 1):
+                c_voxels.append(int(tv_split[i][0]))
+                c_scalar_fracs.append(float(tv_split[i][1]))
+            else:
+                raise Exception("ERROR: unable to read triangle_voxels.dat")
+    tri_voxs = {triangle_ids[i] : owned_voxels[i] for i in range(len(triangle_ids))}
+    tri_sfracs = {triangle_ids[i] : owned_sfracs[i] for i in range(len(triangle_ids)) }
+
+    for tv in tri_voxs.values():
+        for v in range(len(tv)):
+            for v2 in range(v+1, len(tv)):
+                if tv[v] == tv[v2]:
+                    raise Exception("ERROR: surface voxel double-assigned to a triangle")
+    return tri_voxs,tri_sfracs
